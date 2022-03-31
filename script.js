4f64c90ea04303399746320962b1ce595e977d8c
Andrew <lewisdarby2626@gmail.com>
2022-03-30 23:06:54 -0700

    added basic UI

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const rockButton = document.querySelector(".rock-btn");
+const paperButton = document.querySelector(".paper-btn");
+const scissorsButton = document.querySelector(".scissors-btn");
+const clearbutton = document.querySelector(".clear");
+const scoreText = document.querySelector(".score-text");
+const winner = document.querySelector(".winner")
+let results = document.querySelector(".results");
+let callCount = 0;
+let playerTrackerScore = 0;
+let computerTrackerScore = 0;
+results.scrollTop = results.scrollHeight;
+
 // Create function that returns either  ‘Rock’, ‘Paper’ or ‘Scissors’
 
 /*  -> function() {
@@ -39,37 +51,122 @@ function playRound(playerSelection, computerSelection) {
   const rock = "rock";
   const paper = "paper";
   const scissors = "scissors";
-  console.log(`Your choice: ${playerSelection}`);
-  console.log(`Computer's choice: ${computerSelection}`);
+  results.innerText += `Your choice: ${playerSelection}   \n`;
+  results.innerText += `Computer's choice: ${computerSelection}   \n`;
   if (playerSelection === rock && computerSelection === paper) {
-    console.log("You lose! Paper beats Rock");
+    results.innerText += "You lose! Paper beats Rock \n";
+    results.innerText += "\n";
     return "You lose! Paper beats Rock";
   } else if (playerSelection === paper && computerSelection === rock) {
-    console.log("You win! Paper beats Rock");
+    results.innerText += "You win! Paper beats Rock \n";
+    results.innerText += "\n";
     return "You win! Paper beats Rock";
   } else if (playerSelection === scissors && computerSelection === paper) {
-    console.log("You win! Scissors beats Paper");
+    results.innerText += "You win! Scissors beats Paper \n";
+    results.innerText += "\n";
     return "You win! Scissors beats Paper";
   } else if (playerSelection === paper && computerSelection === scissors) {
-    console.log("You lose! Scissors beats Paper");
+    results.innerText += "You lose! Scissors beats Paper \n";
+    results.innerText += "\n";
     return "You lose! Scissors beats Paper";
   } else if (playerSelection === scissors && computerSelection === rock) {
-    console.log("You lose! Rock beats Scissors");
+    results.innerText += "You lose! Rock beats Scissors \n";
+    results.innerText += "\n";
     return "You lose! Rock beats Scissors";
   } else if (playerSelection === rock && computerSelection === scissors) {
-    console.log("You win! Rock beats Scissors");
+    results.innerText += "You win! Rock beats Scissors \n";
+    results.innerText += "\n";
     return "You win! Rock beats Scissors";
   } else if (playerSelection === rock && computerSelection === rock) {
-    console.log("Tie! Rock negates Rock");
+    results.innerText += "Tie! Rock negates Rock \n";
+    results.innerText += "\n";
     return "Tie! Rock negates Rock";
   } else if (playerSelection === paper && computerSelection === paper) {
-    console.log("Tie! Paper negates Paper");
+    results.innerText += "Tie! Paper negates Paper \n";
+    results.innerText += "\n";
     return "Tie! Paper negates Paper";
   } else if (playerSelection === scissors && computerSelection === scissors) {
-    console.log("Tie! Scissors negates Scissors");
+    results.innerText += "Tie! Scissors negates Scissors \n";
+    results.innerText += "\n";
     return "Tie! Scissors negates Scissors";
   }
 }
+
+function score(round) {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  const scoreArray = [];
+  if (round.includes("win")) {
+    playerScore += 1;
+  } else if (round.includes("lose!")) {
+    computerScore += 1;
+  }
+  console.log(`Computer: ${computerScore}`);
+  console.log(`Player: ${playerScore}`);
+
+
+  scoreArray.push(playerScore, computerScore);
+  console.log(scoreArray);
+  // Push scores to an array, return array use that array as an argument list inside next scoreTracker function call with spread operator and optional parameters
+  return scoreArray;
+}
+
+function scoreTracker(humanS, computerS) {
+  winner.innerText = ''
+  callCount += 1;
+  let scoreTrackerArray = [];
+  playerTrackerScore += humanS;
+  computerTrackerScore += computerS;
+  scoreTrackerArray.push(playerTrackerScore, computerTrackerScore);
+  console.log(
+    `playerTrackerScore ${playerTrackerScore} computerTrackerScore${computerTrackerScore} callCount ${callCount}`
+  );
+  // make it so that this if statement below tracks player/computer score (whoever reaches 5 first instead of callcount)
+  //  and then do the same thing to if statement at start of function
+  if (playerTrackerScore >= 5 || computerTrackerScore >= 5) {
+    playerScore = playerTrackerScore;
+    computerScore = computerTrackerScore;
+    if (computerScore > playerScore) {
+      results.innerText += "Computer wins best 3 out of 5 \n\n";
+      winner.innerText = "Computer Wins"
+
+    } else if (playerScore > computerScore) {
+      results.innerText += "Player wins best 3 out of 5 \n\n";
+      winner.innerText = "Player Wins"
+      
+    }
+    callCount = 0;
+    playerTrackerScore = 0;
+    computerTrackerScore = 0;
+  }
+  scoreText.innerText = `Score--  Player:  ${playerTrackerScore} Computer:  ${computerTrackerScore}`;
+
+}
+
+rockButton.addEventListener("click", () => {
+  const scores = score(playRound("Rock", computerPlay()));
+  scoreTracker(...scores);
+});
+paperButton.addEventListener("click", () => {
+  const scores = score(playRound("Paper", computerPlay()));
+  scoreTracker(...scores);
+});
+scissorsButton.addEventListener("click", () => {
+  const scores = score(playRound("Scissors", computerPlay()));
+  scoreTracker(...scores);
+});
+clearbutton.addEventListener("click", () => {
+  results.innerText = "";
+});
+
+// Player: Computer:
+// playRound("scissors", computerPlay());
+
+// game(playRound);
+// Two parameters the playerSelection and computerSelection
+// Then return a string that declares winner make playerSelection parameter case-insensitive
+
 function game(playRound) {
   let round;
   let playerScore = 0;
@@ -108,8 +205,3 @@ function game(playRound) {
     console.log("It is a tie");
   }
 }
-// playRound("scissors", computerPlay());
-
-game(playRound);
-// Two parameters the playerSelection and computerSelection
-// Then return a string that declares winner make playerSelection parameter case-insensitive
